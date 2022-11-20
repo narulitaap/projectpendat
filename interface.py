67065df6f00287fb81cd4870f6334ba6a69894e0
Narulita Arien Pramesti <101122165+narulitaap@users.noreply.github.com>
2022-11-20 21:32:16 +0700

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import re
+import pandas as pd 
+import numpy as np
+
+st.title("""
+form input data, by Arin Comel
+""")
+
+#Fractional Knapsack Problem
+#Getting input from user
+kehamilan=int(st.number_input("Berapa Kali Hamil: ",0))
+glukosa=int(st.number_input("Berapa Konsentrasi Glukosa Plasma : ",0))
+tekanandarah=int(st.number_input("Berapa Tekanan darah diastolik : ",0))
+ketebalankulit=int(st.number_input("Berapa Ketebalan lipatan kulit trisep : ",0))
+insulin=str(st.number_input(" Berapa Insulin serum: ",'0'))
+bmi=str(st.number_input(" Indeks massa tubuh : ",'0'))
+age=str(st.number_input(" Masukkan Usia : ",'0'))
+submit = st.button("submit")
+
+
+if submit:
+    st.info("Jadi,dinyakataan . ")
+
+
