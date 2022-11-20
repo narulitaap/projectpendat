3cfccc34612ed81aece858a6e4d43dcea71c9031
Narulita Arien Pramesti <101122165+narulitaap@users.noreply.github.com>
2022-11-20 21:58:21 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -13,9 +13,9 @@ kehamilan=int(st.number_input("Berapa Kali Hamil: ",0))
 glukosa=int(st.number_input("Berapa Konsentrasi Glukosa Plasma : ",0))
 tekanandarah=int(st.number_input("Berapa Tekanan darah diastolik : ",0))
 ketebalankulit=int(st.number_input("Berapa Ketebalan lipatan kulit trisep : ",0))
-insulin=int(st.number_input(" Berapa Insulin serum: ",'0'))
-bmi=int(st.number_input(" Indeks massa tubuh : ",'0'))
-age=int(st.number_input(" Masukkan Usia : ",'0'))
+insulin=int(st.number_input(" Berapa Insulin serum: ",0))
+bmi=int(st.number_input(" Indeks massa tubuh : ",0))
+age=int(st.number_input(" Masukkan Usia : ",0))
 submit = st.button("submit")
 
 
